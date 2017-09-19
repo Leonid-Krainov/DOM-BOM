c313ac6679025fb62c51eeb41abb1e800b4d7a5d
Leonid-Krainov <KrajnovLeonid@gmail.com>
2017-09-19 21:52:19 +0300

    Done tasks 1-3

---
+++ Task1/skrypt.js
@@ -0,0 +1,23 @@
+var form = document.createElement("form");
+document.body.appendChild(form);
+form.innerHTML = '<input type="text" name="age"><br>';
+form.innerHTML += '<input type="text" name="username"><br>';
+form.innerHTML += '<input type="text" name="date"><br>';
+form.innerHTML += '<input type="button" value="Validate Me"><br>';
+document.querySelector("input[name=age]").setAttribute("placeholder", "age");
+document.querySelector("input[name=username]").setAttribute("placeholder", "username user_...");
+document.querySelector("input[name=date]").setAttribute("placeholder", "date DD/MM/YYYY");
+document.querySelector("body form input[type=button]").onclick = function () {
+    var age = document.querySelector("body form input[name=age]").value;
+    var userName = document.querySelector("body form input[name=username]").value;
+    var date = document.querySelector("body form input[name=date]").value;
+    if (age == "" || age.match(/\D/g) || age<0) {
+        alert("Your data is invalid, wrong age!");
+    }
+    if (!userName.match(/^user_/g)) {
+        alert("Your data is invalid, wrong username!");       
+    }   
+    if (date !== moment().format('DD/MM/YYYY')) {
+        alert("Your data is invalid, wrong date!");  
+    } 
+};
---
+++ Task4/skrypt.js
@@ -0,0 +1,33 @@
+
+/*var getCurrentLanguage = function () {
+	var defaultLanguage = 'ua';
+	return defaultLanguage;
+}*/
+var radio = document.querySelectorAll("div#languages label input");
+for (var k=0; k<radio.length; k++) {
+	radio[k].onclick = function(){
+		var dell = document.getElementsByClassName('visible');
+		console.log(dell);
+		for (var j=0; j<dell.length; j++) {
+			var invisible = dell[j];
+			invisible.classList.remove('visible');
+			console.log(j);
+		}
+		var currentLang = this.value;
+		var langEls = document.getElementsByClassName('lang-' + currentLang);
+		for (var i=0; i<langEls.length; i++) {
+			var langEl = langEls[i];
+
+			langEl.classList.add('visible');
+		}
+	};
+}
+
+
+
+// $<prefix> == DOMElement
+var $save = document.querySelector('html body button#save')
+
+$save.addEventListener('click', function(){
+	alert(1);
+});
