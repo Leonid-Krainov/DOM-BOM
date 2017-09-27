862f26c0787c415fbab0e110b2e1b754d7b33b19
Leonid-Krainov <KrajnovLeonid@gmail.com>
2017-09-27 22:32:50 +0300

    Done task 4

--- Task4/skrypt.js
+++ Task4/skrypt.js
@@ -1,16 +1,18 @@
-defaultLanguage = navigator.language.toLocaleLowerCase();
+if (localStorage.getItem("lang") === null) {
+    defaultLanguage = navigator.language.toLocaleLowerCase();
+} else {
+    defaultLanguage = localStorage.getItem("lang");   
+}
 var currentLanguage = document.querySelectorAll("span.lang-" + defaultLanguage);
 currentLanguage[0].classList.add('visible');
 currentLanguage[1].classList.add('visible');
+
 var radio = document.querySelectorAll("div#languages label input");
 for (var k=0; k<radio.length; k++) {
 	radio[k].onclick = function(){
 		var dell = document.getElementsByClassName('visible');
-		console.log(dell);
-		for (var j=0; j<dell.length; j++) {
-			var invisible = dell[j];
-			invisible.classList.remove('visible');
-			console.log(j);
+		while (dell.length) {
+			dell[0].classList.remove('visible');
 		}
 		var currentLang = this.value;
 		var langEls = document.getElementsByClassName('lang-' + currentLang);
@@ -18,14 +20,10 @@ for (var k=0; k<radio.length; k++) {
 			var langEl = langEls[i];
 			langEl.classList.add('visible');
 		}
-	};
+	}
 }
-
-
-
-// $<prefix> == DOMElement
-var $save = document.querySelector('html body button#save')
-
-$save.addEventListener('click', function(){
-	alert(1);
+var save = document.querySelector('html body button#save')
+save.addEventListener('click', function(){
+	defaultLanguage = document.querySelector("input:checked");
+	localStorage.setItem ("lang", defaultLanguage.value);
 });
